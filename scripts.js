c034e0339de4cc65c40bc843fdfdd98f1846f422
Trevor Pickelheimer <trevorpic@frontier.com>
2016-11-08 14:34:54 -0500

    Created Javascript file. Added functions and constants that pertain to the Register page and the newly added logout button on the Home page. The register button and logout button now link to between the Home and Register pages. Added an error div element to the Register page and the css styling for error div elements.

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const ACCOUNT = "account";
+const ACCOUNT_NUM = "account number";
+const LOGIN = "login";
+const IN = "logged in";
+const OUT = "logged out";
+
+function GetId(id){
+    return document.getElementById(id);
+}
+
+function RegisterLoad(){
+    if(localStorage.getItem(LOGIN) == IN){
+       window.location.href= "Home.html";
+    }
+}
+
+function Register(){
+    var accountNum = 0;
+    if(localStorage.getItem(ACCOUNT_NUM) != null){
+        accountNum = localStorage.getItem("accountNum");
+    }
+    if (GetId("txtRegister").value != ""){
+        localStorage.setItem(ACCOUNT + accountNum, GetId("txtRegister").value);
+        accountNum++;
+        localStorage.setItem(ACCOUNT_NUM, accountNum);
+
+        var loggedIn = IN;
+        localStorage.setItem(LOGIN, loggedIn);
+
+        window.location.href= "Home.html";
+    }else{
+        GetId("divRegisterError").innerHTML = "*Please enter a name.";
+    }
+
+}
+
+function Logout(){
+    localStorage.setItem(LOGIN, OUT);
+    window.location.href = "Register.html"
+}
